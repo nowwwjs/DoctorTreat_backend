9188e0b1e8de9ca1a88c14541aaa477f5d396b72
ahseonga <ahseonga@naver.com>
2024-09-05 09:44:08 +0900

    240905 송아성mapper.xml 파일 추가

--- workspace/src/main/java/com/doctorTreat/app/doctor/DoctorFrontController.java
+++ workspace/src/main/java/com/doctorTreat/app/doctor/DoctorFrontController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.doctorTreat.app.doctor;
 
 import java.io.IOException;
@@ -51,57 +50,3 @@ public class DoctorFrontController extends HttpServlet {
    }
 
 }
-=======
-package com.doctorTreat.app.doctor;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class MemberFrontController
- */
-public class DoctorFrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DoctorFrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
-	}
-	
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String target = request.getRequestURI().substring(request.getContextPath().length());
-		System.out.println(target);
-		switch(target) {
-		case "/doctorjoin.do":
-			request.getRequestDispatcher("/app/user/doctorJoin.jsp").forward(request, response);
-			System.out.println("회원가입 중");
-			break;
-		case "/doctorjoinOk.do":
-			System.out.println("회원가입 완료");
-			break;
-		}
-	}
-
-}
->>>>>>> sas
