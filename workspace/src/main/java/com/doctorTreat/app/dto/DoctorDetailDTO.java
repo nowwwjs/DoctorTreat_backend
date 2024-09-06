d76c06d3f8556f67c9ce2d8befa85eb8d5cfea02
jeong1994 <shjeong19943@gmail.com>
2024-09-06 12:44:19 +0900

    240906 1243 정승환 승진님 파일pr 받기 전 커밋

--- workspace/src/main/java/com/doctorTreat/app/dto/DoctorDetailDTO.java
+++ workspace/src/main/java/com/doctorTreat/app/dto/DoctorDetailDTO.java
@@ -1,23 +1,85 @@
 package com.doctorTreat.app.dto;
 
 public class DoctorDetailDTO {
-//	SELECT DH.HOSPITAL_NAME ,
-//	DD.DOCTOR_NAME ,
-//	DD.DOCTOR_MAJOR ,
-//	DH.HOSPITAL_WORK_TIME ,
-//	DH.HOSPITAL_LUNCH_TIME ,
-//	DH.HOSPITAL_REST_TIME ,
-//	DH.HOSPITAL_NIGHT_WORK ,
-//	DH.HOSPITAL_CALL 
-//FROM DT_DOCTOR DD JOIN DT_HOSPITAL DH 
-//ON DD.HOSPITAL_NUMBER = DH.HOSPITAL_NUMBER
-//WHERE ;
-	String hospitalName;
-	String doctorName;
-	String doctorMajor;
-	String hospitalWorkTime;
-	String hospitalLunchTime;
-	String hospitalRestTime;
-	String hospitalNightWork;
-	String hospitalCall;
+	private String hospitalName;
+	private String doctorName;
+	private String doctorMajor;
+	private String hospitalWorkTime;
+	private String hospitalLunchTime;
+	private String hospitalRestTime;
+	private String hospitalNightWork;
+	private String hospitalCall;
+
+	public String getHospitalName() {
+		return hospitalName;
+	}
+
+	public void setHospitalName(String hospitalName) {
+		this.hospitalName = hospitalName;
+	}
+
+	public String getDoctorName() {
+		return doctorName;
+	}
+
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+
+	public String getDoctorMajor() {
+		return doctorMajor;
+	}
+
+	public void setDoctorMajor(String doctorMajor) {
+		this.doctorMajor = doctorMajor;
+	}
+
+	public String getHospitalWorkTime() {
+		return hospitalWorkTime;
+	}
+
+	public void setHospitalWorkTime(String hospitalWorkTime) {
+		this.hospitalWorkTime = hospitalWorkTime;
+	}
+
+	public String getHospitalLunchTime() {
+		return hospitalLunchTime;
+	}
+
+	public void setHospitalLunchTime(String hospitalLunchTime) {
+		this.hospitalLunchTime = hospitalLunchTime;
+	}
+
+	public String getHospitalRestTime() {
+		return hospitalRestTime;
+	}
+
+	public void setHospitalRestTime(String hospitalRestTime) {
+		this.hospitalRestTime = hospitalRestTime;
+	}
+
+	public String getHospitalNightWork() {
+		return hospitalNightWork;
+	}
+
+	public void setHospitalNightWork(String hospitalNightWork) {
+		this.hospitalNightWork = hospitalNightWork;
+	}
+
+	public String getHospitalCall() {
+		return hospitalCall;
+	}
+
+	public void setHospitalCall(String hospitalCall) {
+		this.hospitalCall = hospitalCall;
+	}
+
+	@Override
+	public String toString() {
+		return "DoctorDetailDTO [hospitalName=" + hospitalName + ", doctorName=" + doctorName + ", doctorMajor="
+				+ doctorMajor + ", hospitalWorkTime=" + hospitalWorkTime + ", hospitalLunchTime=" + hospitalLunchTime
+				+ ", hospitalRestTime=" + hospitalRestTime + ", hospitalNightWork=" + hospitalNightWork
+				+ ", hospitalCall=" + hospitalCall + "]";
+	}
+
 }
