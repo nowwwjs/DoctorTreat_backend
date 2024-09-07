34aecb6b15060e929118d908d26e67afaf56e3d5
jeong1994 <shjeong19943@gmail.com>
2024-09-07 17:36:13 +0900

    240907 1735 정승환 일반회원 채팅목록 수정중

--- workspace/src/main/java/com/doctorTreat/app/doctorClinic/ChatListDoctorController.java
+++ workspace/src/main/java/com/doctorTreat/app/doctorClinic/ChatListDoctorController.java
@@ -1,5 +1,23 @@
 package com.doctorTreat.app.doctorClinic;
 
-public class ChatListDoctorController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.doctorTreat.app.Execute;
+import com.doctorTreat.app.Result;
+
+public class ChatListDoctorController implements Execute {
+	@Override
+	public Result execute(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+		// 결과처리
+        Result result = new Result();
+        result.setRedirect(true);
+        result.setPath("/app/clinic/chatListDoctor.jsp");
+        return result;
+	}
 }
