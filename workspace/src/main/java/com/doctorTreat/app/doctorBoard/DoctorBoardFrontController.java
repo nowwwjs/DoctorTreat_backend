50127c0586f502055d7c7234b11634c44fd8e4bd
ahseonga <ahseonga@naver.com>
2024-09-08 17:13:04 +0900

    240908 송아성파일수정중

--- workspace/src/main/java/com/doctorTreat/app/doctorBoard/DoctorBoardFrontController.java
+++ workspace/src/main/java/com/doctorTreat/app/doctorBoard/DoctorBoardFrontController.java
@@ -53,10 +53,11 @@ public class DoctorBoardFrontController extends HttpServlet {
 			System.out.println("의료 게시판 보이기");
 			System.out.println("안녕");
 			break;
-		case "/BoardDetailDoctor.docbo":
-			result = new BoardDetailDoctorController().execute(request, response);
-			request.getRequestDispatcher(result.getPath()).forward(request, response);
-			break;
+//		case "/BoardDetailDoctor.docbo":
+//			result = new DoctorBoardDetailListController().execute(request, response);
+//			request.getRequestDispatcher(result.getPath()).forward(request, response);
+//			System.out.println("왜 안되는거지?");
+//			break;
 		}
 
 			
--- workspace/src/main/java/com/doctorTreat/app/doctorBoard/dao/DoctorBoardDAO.java
+++ workspace/src/main/java/com/doctorTreat/app/doctorBoard/dao/DoctorBoardDAO.java
@@ -8,14 +8,19 @@ import com.doctorTreat.app.dto.DoctorBoardDTO;
 import com.mybatis.config.MyBatisConfig;
 
 public class DoctorBoardDAO {
-	private SqlSession sqlSession;
+    private SqlSession sqlSession;
 
-	public DoctorBoardDAO() {
-		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
-	}
+    public DoctorBoardDAO() {
+        sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+    }
 
-	public List<DoctorBoardDTO> showList() {
-		return sqlSession.selectList("DoctorBoard.showlist");
-	}
+    // 게시글 목록 조회
+    public List<DoctorBoardDTO> showList() {
+        return sqlSession.selectList("DoctorBoard.showList");
+    }
 
+//    // 회원 번호로 게시글 상세 조회
+//    public List<DoctorBoardDTO> showDetail(int memberNumber) {
+//        return sqlSession.selectList("DoctorBoard.showDetail", memberNumber);
+//    }
 }
