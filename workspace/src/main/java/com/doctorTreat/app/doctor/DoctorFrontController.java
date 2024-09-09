17a9d18275ac9ca869fca0a3d35847909b209202
jeong1994 <shjeong19943@gmail.com>
2024-09-09 11:07:38 +0900

    240909 1107 풀받기 전 커밋

--- workspace/src/main/java/com/doctorTreat/app/doctor/DoctorFrontController.java
+++ workspace/src/main/java/com/doctorTreat/app/doctor/DoctorFrontController.java
@@ -14,57 +14,6 @@ import com.doctorTreat.app.memberClinic.ChatListMemberController;
  * Servlet implementation class MemberFrontController
  */
 public class DoctorFrontController extends HttpServlet {
-<<<<<<< HEAD
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public DoctorFrontController() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doProcess(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doProcess(request, response);
-	}
-
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String target = request.getRequestURI().substring(request.getContextPath().length());
-		System.out.println(target);
-		Result result = null;
-
-		switch (target) {
-		case "/doctor/doctorJoin.do":
-			request.getRequestDispatcher("/app/user/doctorJoin.jsp").forward(request, response);
-			System.out.println("회원가입 중");
-			break;
-
-		case "/doctor/doctorJoinOk.do":
-			result = new DoctorJoinController().execute(request, response);
-			request.getRequestDispatcher(result.getPath()).forward(request, response);
-			break;
-
-		}
-	}
-
-}
-=======
    private static final long serialVersionUID = 1L;
 
    /**
@@ -133,4 +82,3 @@ public class DoctorFrontController extends HttpServlet {
       }
    }
 }
->>>>>>> main
