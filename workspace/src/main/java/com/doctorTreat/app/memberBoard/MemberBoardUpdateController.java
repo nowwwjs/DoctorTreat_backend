713b85cbe3f7c426cff9b67773db9f2a999991ad
nowwwjs <jaeseo1227@gmail.com>
2024-09-09 15:50:15 +0900

    240909 15:50 이재서

--- workspace/src/main/java/com/doctorTreat/app/memberBoard/MemberBoardUpdateController.java
+++ workspace/src/main/java/com/doctorTreat/app/memberBoard/MemberBoardUpdateController.java
@@ -13,32 +13,34 @@ import com.doctorTreat.app.memberBoard.dao.MemberBoardDAO;
 
 public class MemberBoardUpdateController implements Execute {
 
-	@Override
-	public Result execute(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		request.setCharacterEncoding("UTF-8");
-		Result result = new Result();
+    @Override
+    public Result execute(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        Result result = new Result();
 
-		System.out.println("무임");
+        MemberBoardDAO memberBoardDAO = new MemberBoardDAO();
 
-		MemberBoardDAO memberBoardDAO = new MemberBoardDAO();
+        // 폼에서 전달된 데이터 가져오기
+        String updateTitle = request.getParameter("updateTitle");
+        String updateText = request.getParameter("updateText");
+        String sessionNumber = (String) request.getSession().getAttribute("medicalInfoNumber");
+        
+        // 세션에서 게시글 번호 가져오기
+        int medicalInfoNumber = Integer.valueOf(sessionNumber); 
 
+        // DTO 객체 생성 및 값 설정
+        MemberBoardDTO memberBoardDTO = new MemberBoardDTO();
+        memberBoardDTO.setMedicalInfoNumber(medicalInfoNumber);
+        memberBoardDTO.setMedicalInfoTitle(updateTitle);
+        memberBoardDTO.setMedicalInfoText(updateText);
 
-		// 폼에서 전달된 데이터 가져오기
-		String updateTitle = request.getParameter("updateTitle");
-		String updateText = request.getParameter("updateText");
-		String sessionNumber = (String) request.getSession().getAttribute("medicalInfoNumber");
-		
-		int medicalInfoNumber = Integer.valueOf(sessionNumber); 
-		
-		System.out.println(medicalInfoNumber);
+        // DAO 호출하여 업데이트 수행
+        memberBoardDAO.update(memberBoardDTO);
 
-		/* memberBoardDAO.update(updateTitle , updateText , medicalInfoNumber); */
+        // 결과 설정
+        result.setRedirect(true);
 
-		// 결과 설정
-		result.setRedirect(true);
-
-		return result;
-	}
-
+        return result;
+    }
 }
