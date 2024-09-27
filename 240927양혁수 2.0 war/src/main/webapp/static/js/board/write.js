5a69ba815083ee917d8093fe593ac525e959d374
jeong1994 <shjeong19943@gmail.com>
2024-09-27 12:10:18 +0900

    240927 1210 정승환 채팅 완성 최종pr

--- 240927양혁수 2.0 war/src/main/webapp/static/js/board/write.js
+++
@@ -1,7 +0,0 @@
-// 게시글 등록 버튼 클릭 시 상세 페이지로 이동
-// 게시글 등록 버튼 클릭 시 메시지 표시
-/*document.querySelector('.member-write-register').addEventListener('click', function(event) {
-    event.preventDefault();
-    alert("등록하시겠습니까?");
-});
-*/
